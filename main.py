7cda0f10d1f944936c3ff58f65d3ccdf4dea05e6
odevpedro <pedrosschmidt2@gmail.com>
2023-11-27 08:50:41 -0300

    course overview

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pyspark
 from pyspark.sql.types import IntegerType
-from pyspark.sql import SparkSession
+
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    
+    print(f'Hi, {name}')  
+    print(f'Hello, {name}')  
     my_list = [1,2,3]
     spark = SparkSession.builder.appName("my first spark app").enableHiveSupport().getOrCreate()
     df = spark.createDataFrame(my_list,IntegerType())
@@ -19,10 +15,6 @@ def print_hi(name):
     spark.sql("create table as select * from temptable1")
 
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Python Pycharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,2 @@
+#Course Overview 
+This course outlines the creation of a data pipeline in Python. It introduces a project structure involving three main components: ingestion, transformation, and persistence. The script establishes classes for each component, demonstrates class instantiation, and orchestrates the pipeline's execution. The goal is to gradually enhance functionality within each component using PySpark for data processing..
